2c6da6bdb98c6a20c44e145edf93fd82bef40468
Yago Ananias <yago_souza92@yahoo.com.br>
2020-04-20 05:38:05 -0300

    Front-end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './global.css';
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React.
-        </p>
-        <a
-          className="App-link"
-          href="https://yagoananias.com.br"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Chama Nois
-        </a>
-      </header>
-    </div>
+    <Routes/>
   );
 }
 
